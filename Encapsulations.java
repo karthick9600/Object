54a0d63b558200c6cfe0d7f91676a64eeb8a7836
karthick9600 <146514482+karthick9600@users.noreply.github.com>
2025-05-13 18:09:08 +0530

    Add files via upload

---
+++ Encapsulations.java
@@ -0,0 +1,39 @@
+package com.Project;
+
+ class Encapsulations {
+	private String name;
+	private String color;
+	private int cost;
+}
+ public void setName(String X) {
+	 name=X; 
+ }
+	public void setColor(String Y) {
+		color=Y;
+	}
+	public void setCost(int z) {
+		cost=z;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public String getColor() {
+		return color;
+	}
+		public String getCost() {
+			return cost;
+		}
+			
+		
+	 
+    public class Encapsulations{
+	public static void main(String[] args) {
+		Encapsulations1 E=new Encapsulations1();
+		E.setName("karthick")
+		
+		
+
+	}
+
+}
