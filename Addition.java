22bf702d3780bdad7c65e57a912472f70661dee0
karthick9600 <146514482+karthick9600@users.noreply.github.com>
2025-05-13 16:16:00 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,24 @@
+package com.Project;
+
+ class Addition1 {
+	float add(int x,float y) {
+		return x+y;
+	}
+	
+	}
+	 public class Addition{
+		public static void main(String[] args) {
+			 Addition1 ad = new Addition1();
+			 System.out.println( ad.add (10,20.5f));
+		
+			 
+			 
+		 
+	 }
+
+		
+		
+
+	}
+
+
